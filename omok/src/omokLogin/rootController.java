c5867298e3a9e95c7edf14e7181fb00c4714bf1b
Park Taeseon <wish_snow@naver.com>
2016-12-15 20:21:26 +0900

    다울3차 commit 회원가입창 가입버튼 처리

--- omok/src/omokLogin/rootController.java
+++ omok/src/omokLogin/rootController.java
@@ -15,14 +15,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class rootController implements Initializable{
@@ -79,6 +75,10 @@ public class rootController implements Initializable{
 				}catch(Exception e){
 					e.printStackTrace();
 				}
+				
+				
+				
+				
 			}
 		});
 		
